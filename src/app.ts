6938fdb9bfd2a311d81fe17a21f2accc23eb8427
Greg Pluta <pluta7@gmail.com>
2017-07-04 21:34:41 +0200

    Expand the example

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log(123);
+class Greeter {
+    greeting: string;
+
+    constructor(message: string) {
+        this.greeting = message;
+    }
+
+    greet() {
+        console.log(`Hello, ${this.greeting}!`);
+    }
+}
+
+let greeter = new Greeter("world");
+
+greeter.greet();
