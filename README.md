74c58954061735669be4e1884127b0d36a59b664
Greg Pluta <pluta7@gmail.com>
2017-07-04 21:27:38 +0200

    Rewrite watcher using chokidar

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Super simple ts starter - for node apps - without bundling
 
-- Run `npm run work` to start the watcher
+- Run `npm run watch` to start the watcher
 - Run `npm run build` to build your project from `./src` dir
 - Run `npm run clean` to clean up the mess ;)
 - Work in `./src`
---
+++ watch.js
@@ -0,0 +1,35 @@
+const util = require('util');
+const chalk = require('chalk');
+const chokidar = require('chokidar');
+
+const exec = util.promisify(require('child_process').exec);
+
+const log = console.log;
+const SRC = './src';
+
+const watcher = chokidar.watch(SRC, {
+    ignoreInitial: true
+});
+
+watcher
+    .on('all', (event, path) => {
+        console.log(`${event}: ${path}`);
+        compileTypescript()
+            .catch(e => {
+                console.log(e);
+            });
+    });
+
+async function compileTypescript() {
+    let command = 'node_modules/.bin/tsc';
+    try {
+        let start = new Date();
+        console.log(chalk.green('Compiling...'));
+        await exec(command);
+        console.log(chalk.green(`Compiled in ${(new Date() - start) / 1000} secs`));
+    } catch (e) {
+        throw new Error(e.stdout);
+    }
+}
+
+log(chalk.blue('Watching for changes'));
